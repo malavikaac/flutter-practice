0dafee2f7f61c23b1de4c2666fdee49be0573af0
malavikaac <acmalavika@gmail.com>
2023-02-13 09:58:03 +0530

    expaneded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -17,42 +15,44 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 117, 197, 120),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Row(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('Hello'),
-              Text('Atre'),
-            ],
+          Expanded(child: Image.asset('assets/water.jpg')),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Color.fromARGB(255, 130, 180, 221),
+              padding: EdgeInsets.all(30.0),
+              child: Text('1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  )),
+            ),
           ),
-          Container(
-            color: Color.fromARGB(255, 130, 180, 221),
-            padding: EdgeInsets.all(40.0),
-            child: Text('inside container',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                )),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Color.fromARGB(255, 146, 214, 226),
+              padding: EdgeInsets.all(30.0),
+              child: Text('2',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 14, 13, 13),
+                    fontSize: 25,
+                  )),
+            ),
           ),
-          Container(
-            color: Color.fromARGB(255, 146, 214, 226),
-            padding: EdgeInsets.all(30.0),
-            child: Text('second container',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 14, 13, 13),
-                  fontSize: 25,
-                )),
-          ),
-          Container(
-            color: Color.fromARGB(255, 208, 177, 226),
-            padding: EdgeInsets.all(40.0),
-            child: Text('Third container',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                )),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Color.fromARGB(255, 208, 177, 226),
+              padding: EdgeInsets.all(30.0),
+              child: Text('3',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  )),
+            ),
           ),
         ],
       ),
